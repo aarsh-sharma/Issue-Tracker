e098485a1eda1d1be4d419c7ce4a74b2ebb34460
Aarsh Sharma <aarsh01rsh@gmail.com>
2020-12-26 02:53:36 +0530

    Initial commit

---
+++ tickets/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('list', list, name='list'),
+]
---
+++ tickets/views.py
@@ -0,0 +1,14 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, 'index.html')
+
+
+def list(request):
+
+    return render(request, 'list.html')
